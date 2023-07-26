f3249832744af0786a7e21612ad56dd7d60ce088
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-07-26 06:29:26 -0700

    Attempting advanced tasks: shell sort and cocktail sort

--- sort.h
+++ sort.h
@@ -28,5 +28,8 @@ void swap(int *a, int *b);
 size_t lomuto_partition(int *array, ssize_t low, ssize_t high);
 void quick_sort_recursive(int *array, ssize_t low, ssize_t high);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
+void cocktail_sort_list(listint_t **list);
 
 #endif
